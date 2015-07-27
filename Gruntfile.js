e9f921bdfaefe30860d7be38328325f8ace9f1e9
Rob Uridge <rob@Robs-MacBook-Pro.local>
2015-07-27 12:23:30 +0100

    jekyll template theme and grunt server

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function(grunt) {
+
+  // All configuration goes here
+  grunt.initConfig({
+
+    jekyll: {
+      build : {
+        dest: '_site'
+      }
+    },
+
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['*.scss'],
+          dest: '_site/css',
+          ext: '.css'
+        }]
+      }
+    },
+
+    watch: {
+      sass: {
+        files: ['_sass/*.scss', '_sass/*/*.scss', 'css/*.scss'],
+        tasks: ['sass']
+      },
+      jekyll: {
+        files: ['_layouts/*.html', '_includes/*.html'],
+        tasks: ['jekyll']
+      }
+    },
+
+    browserSync: {
+      bsFiles: {
+        src : ['_site/css/*.css']
+      },
+      options: {
+        watchTask: true,
+        ghostMode: {
+          clicks: true,
+          scroll: true,
+          links: true,
+          forms: true
+        },
+        server: {
+          baseDir: '_site'
+        }
+      }
+    }
+
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-jekyll');
+  grunt.loadNpmTasks('grunt-browser-sync');
+
+  // Custom tasks
+  grunt.registerTask('build', ['jekyll', 'sass']);
+  grunt.registerTask('default', ['build', 'browserSync', 'watch']);
+};
